79a3c34d6c66c0555477d3cab9af576cd59aa5d9
Igor Liakh <igorlyah060@gmail.com>
2023-09-12 00:49:43 +0300

    done task 8

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,19 @@
+const form = document.querySelector(".login-form")
 
+form.addEventListener("submit", e => {
+	e.preventDefault()
+
+	const email = form.elements.email
+	const password = form.elements.password
+
+	if (email.value === "" || password === "") {
+		alert("All fields must be filled in")
+	} else {
+		const data = {
+			email: email.value,
+			password: password.value,
+		}
+		console.log(data)
+		form.reset()
+	}
+})
