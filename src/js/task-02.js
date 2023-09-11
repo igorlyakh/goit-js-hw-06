e64a1722b0055ec2e8022f482cd44ca131741f54
Igor Liakh <igorlyah060@gmail.com>
2023-09-11 21:16:30 +0300

    done task 2

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -1,8 +1,19 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
-];
+	"Potatoes",
+	"Mushrooms",
+	"Garlic",
+	"Tomatos",
+	"Herbs",
+	"Condiments",
+]
+
+const list = document.querySelector("#ingredients")
+
+// const li = document.createElement("li")
+
+ingredients.forEach(item => {
+	const li = document.createElement("li")
+	li.textContent = item
+	li.setAttribute("class", "item")
+	list.append(li)
+})
