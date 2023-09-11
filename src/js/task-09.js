2719449362fc6fe0432677d596b7a815ca6dc618
Igor Liakh <igorlyah060@gmail.com>
2023-09-12 00:53:25 +0300

    done task 9

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,5 +1,15 @@
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215)
-    .toString(16)
-    .padStart(6, 0)}`;
+	return `#${Math.floor(Math.random() * 16777215)
+		.toString(16)
+		.padStart(6, 0)}`
 }
+
+const widget = document.querySelector(".widget")
+const color = document.querySelector(".color")
+const changeBtn = document.querySelector(".change-color")
+
+changeBtn.addEventListener("click", () => {
+	const randomColor = getRandomHexColor()
+	widget.style.backgroundColor = randomColor
+	color.textContent = randomColor
+})
