50a85acc82878d5951bf24f81a50f8d367d6d7a0
Igor Liakh <igorlyah060@gmail.com>
2023-09-12 21:56:09 +0300

    fix task 2

--- js/task-02.js
+++ js/task-02.js
@@ -9,11 +9,11 @@ const ingredients = [
 
 const list = document.querySelector("#ingredients")
 
-// const li = document.createElement("li")
-
-ingredients.forEach(item => {
+const items = ingredients.map(item => {
 	const li = document.createElement("li")
 	li.textContent = item
 	li.setAttribute("class", "item")
-	list.append(li)
+	return li
 })
+
+list.append(...items)
